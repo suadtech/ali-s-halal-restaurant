4c6d51fb241bc08a29d5cbb92ce5cb2b54aab254
suadtech <suadtech1@gmail.com>
2025-04-09 09:16:29 +0000

    create templates-booking fils

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from django.contrib import admin
+from .models import MenuCategory, MenuItem
 # Register your models here.
+admin.site.register(MenuCategory)
+admin.site.register(MenuItem)
--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from .models import MenuCategory, MenuItem
+from django.shortcuts import render, get_object_or_404
+from .models import MenuCategory, MenuItem
 
 # Create your views here.
+def menu_list(request):
+    categories = MenuCategory.objects.all()
+    menu_items = MenuItem.objects.filter(is_available=True)
+    
+    return render(request, 'menu/menu_list.html', {
+        'categories': categories,
+        'menu_items': menu_items
+    })
+def category_detail(request, category_id):
+    category = get_object_or_404(MenuCategory, id=category_id)
+    menu_items = MenuItem.objects.filter(category=category, is_available=True)
+    
+    return render(request, 'menu/category_detail.html', {
+        'category': category,
+        'menu_items': menu_items
+    })
